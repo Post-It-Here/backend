1dc1ebd47217ba6164673789b55f13f18e1e8485
tyler broadway <tbroadway96@gmail.com>
2020-06-17 22:16:12 -0500

    Added foreign keys

--- database/migrations/20200617203654_table-creator.js
+++ database/migrations/20200617203654_table-creator.js
@@ -1,20 +1,24 @@
 exports.up = function(knex) {
   return knex.schema
     .createTable('users', users => {
-        users.increments('id');
+        users.increments('id').notNullable();
         users.string('username', 128).notNullable();
         users.string('password', 128).notNullable();
     })
     .createTable('posts', posts => {
-        posts.increments('id');
+        posts.increments('id')
+            .notNullable()
+            .references('id')
+            .inTable('users');
         posts.string('title', 255).notNullable();
         posts.string('description', 500).notNullable();
-        // posts.foreign('id').references('users.id');
     })
     .createTable('subs', rec => {
-        rec.increments('id');
+        rec.increments('id')
+            .notNullable()
+            .references('id')
+            .inTable('posts');
         rec.specificType('subreddits', 'string ARRAY');
-        // rec.foreign('id').references('posts.id');
     })
 };
 
