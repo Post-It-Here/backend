e3dbcbf1a402c7d585cb2996358964324ab238b8
tyler broadway <tbroadway96@gmail.com>
2020-06-18 21:13:22 -0500

    Created .env file & built out server.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const server = require('express')();
 const PORT = process.env.PORT || 5000;
 
--- server/api/server.js
+++ server/api/server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const authenticate = require('../auth/authenticate');
 const authRouter = require('../routes/authRouter');
+const appRouter = require('../routes/appRouter');
 
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-server.use('/api/', authRouter);
+server.use('/api', authRouter);
+server.use('/api', authenticate, appRouter);
 
 server.get('/', () => {
     res.send('Server is running');
-})
+});
